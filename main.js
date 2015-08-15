7fd04f350d499fae346a7822a3d4a8be022f06db
mary <grimesmea@gmail.com>
2015-08-14 23:27:02 -0400

    Add Knockout.js framework Modify interface

--- main.js
+++ main.js
@@ -1,27 +1,51 @@
-function rotate13Decode(secretMessage) {
-  this.secretMessage = secretMessage;
-  var secretMessageArray = secretMessage.split('');
-  var decodedMessageArray = [];
+var MyInput = {
+  inputMessage: null,
 
-  for(var i = 0; i < secretMessageArray.length; i++) {
-    var letterCode = secretMessageArray[i].charCodeAt();
-    var targetLetterCode = letterCode - 13;
+  init: function(input) {
+    this.inputMessage = input;
+  },
+  rot13: function(message) {
+    this.message = message;
+    var messageArray = this.message.split('');
+    var rotMessageArray = [];
 
-    console.log(letterCode);
+    for(var i = 0; i < messageArray.length; i++) {
+      var letterCode = messageArray[i].charCodeAt();
+      var targetLetterCode = letterCode - 13;
 
-    if(letterCode < 65 ||
-       (letterCode > 90 && letterCode < 97) ||
-       letterCode > 122) {
-      targetLetterCode = letterCode;
-    } else if(targetLetterCode < 65 ||
-              (letterCode >= 97 && targetLetterCode < 97)) {
-      targetLetterCode += 26;
+      if(letterCode < 65 ||
+         (letterCode > 90 && letterCode < 97) ||
+         letterCode > 122) {
+        targetLetterCode = letterCode;
+      } else if(targetLetterCode < 65 ||
+               (letterCode >= 97 && targetLetterCode < 97)) {
+        targetLetterCode += 26;
+      }
+
+      rotMessageArray.push(targetLetterCode);
     }
 
-    decodedMessageArray.push(targetLetterCode);
+    return String.fromCharCode.apply(null, rotMessageArray);
   }
+};
 
-  return String.fromCharCode.apply(null, decodedMessageArray);
-}
+var viewModel = function() {
+  var self = this;
 
-rotate13Decode('Fraq hf gur pbqr lbh hfrq gb qrpbqr guvf zrffntr');
+  self.outputMessage = ko.observable('');
+  self.input = ko.pureComputed({
+    read: function() {
+      self.input;
+    },
+    write: function(value) {
+      self.outputMessage(MyInput.rot13(value));
+    },
+    owner: self
+  });
+
+  MyInput.init(self.input());
+};
+
+$(function() {
+  ko.applyBindings(new viewModel());
+});
